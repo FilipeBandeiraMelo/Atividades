7bf826785e88c1c198b0380869d37cf1c8015202
FilipeBandeiraMelo <107214023+FilipeBandeiraMelo@users.noreply.github.com>
2022-08-08 23:35:54 -0300

    Add files via upload

---
+++ Atividadefuncao_1.js
@@ -0,0 +1,15 @@
+// Crie um programa que contenha os seguintes tipos de funções:
+
+// 1. uma função tradicional com a palavra reservada “Function” e sem nenhum parâmetro;
+function exibirHoraAtual(){
+    console.log(new Date() .getHours())
+}
+exibirHoraAtual();
+//2. uma função tradicional com parâmetros e um retorno de valor;
+function soma(a,b){
+    return a+b;
+}
+console.log(soma(3,5))
+//3. uma arrow function com parâmetros e que retorne um valor.
+const multiplica = (n1,n2) => n1*n2;
+console.log(multiplica(3,3))
