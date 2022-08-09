afb8d0d0cf3434c193c3ca96bc65a4b9bc3e7b60
FilipeBandeiraMelo <107214023+FilipeBandeiraMelo@users.noreply.github.com>
2022-08-08 23:36:54 -0300

    Update Atividadefuncao_1.js

--- Atividadefuncao_1.js
+++ Atividadefuncao_1.js
@@ -2,14 +2,14 @@
 
 // 1. uma função tradicional com a palavra reservada “Function” e sem nenhum parâmetro;
 function exibirHoraAtual(){
-    console.log(new Date() .getHours())
+    console.log(new Date() .getHours());
 }
 exibirHoraAtual();
 //2. uma função tradicional com parâmetros e um retorno de valor;
 function soma(a,b){
     return a+b;
 }
-console.log(soma(3,5))
+console.log(soma(3,5));
 //3. uma arrow function com parâmetros e que retorne um valor.
 const multiplica = (n1,n2) => n1*n2;
-console.log(multiplica(3,3))
+console.log(multiplica(3,3));
