26992909554aecdb8e7be79aa0ecb2ce8db2be55
FilipeBandeiraMelo <107214023+FilipeBandeiraMelo@users.noreply.github.com>
2022-08-24 01:11:10 -0300

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,7 @@
+const palavra="Softex";
+console.log(palavra.toUpperCase()) 
+console.log(palavra.toLowerCase()) 
+
+let login = "   softex@emailteste.com      ";
+let loginSemEspaco = login.trim();
+console.log(loginSemEspaco);
